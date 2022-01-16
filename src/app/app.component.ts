046e1f3c6b59edd8f89ab18425a7799446390d52
giuseppemorale <morale-giuseppe@live.it>
2022-01-16 01:28:55 +0100

    2 files modified, 1 deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Subject } from 'rxjs';
 import { Calciatore } from './models/calciatore.model';
-import { PlayerService } from './services/player.service';
 import { DataTableOptions } from './util/datatable-options.util';
 
 @Component({
@@ -12,11 +11,20 @@ export class AppComponent implements OnInit, OnDestroy {
   dtOptions: any = {};
   dtTrigger: Subject<any> = new Subject<any>();
 
-  public listaCalciatori: Calciatore[] = [];
+  public listaCalciatori: Calciatore[] = [
+    { ruolo: 'P', calciatore: 'Andrenacci', squadra: 'GEN', quotazione: '1' },
+    { ruolo: 'D', calciatore: 'Ceccaroni', squadra: 'VEN', quotazione: '9' },
+    { ruolo: 'D', calciatore: 'Ghoulam', squadra: 'NAP', quotazione: '1' },
+    { ruolo: 'D', calciatore: 'Mazzocchi', squadra: 'VEN', quotazione: '5' },
+    { ruolo: 'C', calciatore: 'Akpa Akpro', squadra: 'LAZ', quotazione: '2' },
+    { ruolo: 'C', calciatore: 'Anderson A.', squadra: 'LAZ', quotazione: '2' },
+    { ruolo: 'C', calciatore: 'Asllani', squadra: 'EMP', quotazione: '2' },
+    { ruolo: 'A', calciatore: 'Afena-Gyan', squadra: 'ROM', quotazione: '3' },
+    { ruolo: 'A', calciatore: 'Antiste', squadra: 'SPE', quotazione: '7' },
+    { ruolo: 'A', calciatore: 'Bianchi F.', squadra: 'GEN', quotazione: '6' },
+  ];
 
-  constructor(public playerService: PlayerService) {
-    this.listaCalciatori = this.playerService.lista;
-  }
+  constructor() {}
 
   public ngOnInit(): void {
     this.dtOptions = DataTableOptions.Italian;
